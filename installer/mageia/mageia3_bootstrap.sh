3098f113c72b3e0389b42d1e092d2f2d5ffcf36d
Matthew <xboxboy@server.fake>
2013-10-31 23:26:47 +1030

    added resol.conf copy line

--- installer/mageia/mageia3_bootstrap.sh
+++ installer/mageia/mageia3_bootstrap.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 
+# Script needs to be run as root: or extraction of filesystem-xxxx.release.arch.rpm will fail, and the install will break.
+
 # This script has a fixed mirror address: Mirror variations to be added!
+# For future reference: url=http://mirrors.mageia.org/api/mageia.(release).(arch).list; wget -q ${url} -O - | grep rsync:       provides list of official mirrors where release & arch suit the local system
 
 # Create chroot directory: # testing in ~ at present
 
-mkdir ~/chroottest
+mkdir /chroottest
 
 # Enter chroottest directory
 
-cd ~/chroottest
+cd /chroottest
 
 # Get latest rpm listing : Again as above, mirror is fixed
 
@@ -36,17 +39,57 @@ chmod 555 bash_cpio.sh
 
 # Determine rpm full name and version and gather rpms for environment: Only supports X86_64 at present: arch detection and response to be added!
 
-for name in $(< ~/chroottest/mageia3_rpms_req.txt);
+for name in $(< /chroottest/mageia3_rpms_req.txt);
 do
-  wgetvar=$(grep "^$name-[0-9]" ~/chroottest/rpmlist.txt;)
+  wgetvar=$(grep "^$name-[0-9]" /chroottest/rpmlist.txt;)
   echo $wgetvar
-  wget --spider http://ftp.aarnet.edu.au/pub/mageia/distrib/3/x86_64/media/core/release/${wgetvar}
+  wget http://ftp.aarnet.edu.au/pub/mageia/distrib/3/x86_64/media/core/release/${wgetvar} -O "$name.rpm"
 done
 
 #Test download filesystem rpm for extraction: Manually
 
-wget http://ftp.aarnet.edu.au/pub/mageia/distrib/3/x86_64/media/core/release/filesystem-2.1.9-20.mga3.x86_64.rpm
+# wget http://ftp.aarnet.edu.au/pub/mageia/distrib/3/x86_64/media/core/release/filesystem-2.1.9-20.mga3.x86_64.rpm
+
+# added a perl depenency
+
+# wget http://mirror.aarnet.edu.au/pub/mageia/distrib/3/x86_64/media/core/release/perl-List-MoreUtils-0.330.0-3.mga3.x86_64.rpm
 
 #filesystem* rpm must be extracted first, or bootstrap filesystem will not suit rpm (package manager)
 
-# ./bash_rpm_cpio.sh filesystem-2.1.9-20.mga3.x86_64.rpm | ./bash_cpio.sh -idm     # Needs to be run as root!
+./bash_rpm_cpio.sh filesystem.rpm | ./bash_cpio.sh -idmv
+
+#All required rpm's to be extracted into filesystem using bash rpm extraction tools
+
+find . -type f -name "*".rpm | xargs -n1 -ifile sh -c "./bash_rpm_cpio.sh file | ./bash_cpio.sh -idv"
+
+# Create /dev in chroot
+
+mkdir /chroottest/dev
+
+#We need to bind to host system directories
+
+mount -o bind /proc /chroottest/proc
+mount -o bind /dev /chroottest/dev
+mount -o bind /sys /chroottest/sys
+
+# Copy host resolve.conf for networking : Needs to overwrite existing file in chroot
+
+cp -fr /etc/resolv.conf /chroottest/etc/resolv.conf
+
+#enter chroot
+
+# chroot /chroottest/
+
+# Use RPM to reinstall all rpms: Build rpm database
+
+# A: find . -type f -name "*".rpm | xargs -n1 -ifile /bin/rpm file -iv --nodeps --replacepkgs
+
+# Urpmi not working due to locale problems : May not need this following export line
+
+# export LC_ALL=en_AU.UTF-8 
+
+# Add mirror list to urpmi
+
+# urpmi.addmedia --distrib --mirrorlist '$MIRRORLIST'
+
+# B: urpmi.addmedia --distrib ftp://ftp.mirror.aarnet.edu.au/pub/mageia/distrib/3/x86_64
